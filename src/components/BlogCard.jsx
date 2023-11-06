e343473256b8fc31b643556d24ee85bf1de05506
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-07 01:54:15 +0600

    Skeleton shown when fetching data

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,19 +1,33 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
 const BlogCard = ({ blog }) => {
   const { title, category, image, short_desc } = blog;
+
   return (
-    <div className="p-5 border shadow rounded flex flex-col justify-between space-y-4">
+    <div className="p-5 border shadow rounded">
       <img
         className="rounded w-full h-48 object-cover"
         src={image}
         alt={title}
       />
-      <span className="w-max px-3 py-1 text-xs font-medium bg-green-600 text-white rounded-full">
-        {category}
-      </span>
-      <h4 className="text-xl font-bold">{title}</h4>
-      <p>{short_desc.split(" ").slice(0, 15).join(" ")}...</p>
+      <div className="flex flex-col justify-between space-y-2 mt-5">
+        <span className="w-max px-3 py-1 text-xs font-medium bg-green-600 text-white rounded-full">
+          {category}
+        </span>
+        <h4 className="text-xl font-bold">{title}</h4>
+        <p>
+          {short_desc.split(" ").slice(0, 15).join(" ")}{" "}
+          <Link to={`/`} className="text-blue-700 font-bold hover:underline">
+            Read More...
+          </Link>
+        </p>
+      </div>
     </div>
   );
+};
+BlogCard.propTypes = {
+  blog: PropTypes.object,
 };
 
 export default BlogCard;
--- src/pages/AllBlogs.jsx
+++ src/pages/AllBlogs.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import BlogCard from "../components/BlogCard";
 import SectionContainer from "../components/SectionContainer";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const AllBlogs = () => {
   const { isLoading, error, data } = useQuery({
@@ -10,10 +12,22 @@ const AllBlogs = () => {
   });
   const blogs = data?.data || [];
   console.log(blogs, isLoading, error);
+
   return (
     <SectionContainer>
       <div className="py-20">
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {isLoading && (
+            <SkeletonTheme count={3}>
+              <p>
+                <Skeleton className="h-48" />
+                <br />
+                <Skeleton />
+                <br />
+                <Skeleton count={3} />
+              </p>
+            </SkeletonTheme>
+          )}
           {blogs?.length > 0 &&
             blogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)}
         </div>
